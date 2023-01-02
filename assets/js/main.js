661cc7260de2692cdcd7e80143280f676321141e
caritonk <mcchavarriaga5@misena.edu.co>
2023-01-02 12:58:03 -0500

    intentando organizar la ruta de los archivo e imagenes para que se vean en github pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,82 @@
-const navbarEmail = document.querySelector(".navbar-email");
-const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
-const navbarBurguerMain = document.querySelector(".burguer-main");
+//Las funciones declarativas, condicionales y ciclos en JavaScript no terminan con ";"
+
+const navEmail = document.querySelector(".navbar-email");
+const navShoppingCart = document.querySelector(".navbar-shopping-cart");
+const navBurguerMain = document.querySelector(".burguer-main");
 const mainDesktop = document.querySelector(".desktop-main");
 const mainMobile = document.querySelector(".mobile-main");
 const shoppingCart = document.querySelector(".shopping-cart");
-const productContainer = document.querySelector(".product-container");
+const productCardContainer = document.querySelector(".product-card-container");
+const productDetails = document.querySelector(".product-details");
+const productDetailsClose = document.querySelector(".product-details-close");
 
 const inactive = "inactive";
-const isMainMobileClosed = mainMobile.classList.contains(inactive);
-const isMainDesktopClosed = mainDesktop.classList.contains(inactive);
-const isShoppingCartClosed = shoppingCart.classList.contains(inactive);
 
-navbarBurguerMain.addEventListener("click",toggleMainMobile);
-navbarEmail.addEventListener("click",toggleMainDesktop);
-navbarShoppingCart.addEventListener("click",toggleShoppingCart);
-
-function toggleMainMobile(){
-    if (isShoppingCartClosed){
-        shoppingCart.classList.add(inactive);
-    };
-    mainMobile.classList.toggle(inactive);
-};
+navBurguerMain.addEventListener("click",toggleMainMobile);
+navEmail.addEventListener("click",toggleMainDesktop);
+navShoppingCart.addEventListener("click",toggleShoppingCart);
+productDetailsClose.addEventListener("click",closeProductDetails);
 
 function toggleMainDesktop(){
-    if (isShoppingCartClosed){
-        shoppingCart.classList.add(inactive);
-    };
+    shoppingCart.classList.add(inactive);
     mainDesktop.classList.toggle(inactive);
-};
+}
+
+function toggleMainMobile(){
+    if(isShoppingCartClosed){
+        shoppingCart.classList.add(inactive);
+    }
+    mainMobile.classList.toggle(inactive);
+}
 
 function toggleShoppingCart(){
-    if (isMainMobileClosed){
-        mainMobile.classList.add(inactive);
-    };
-    if (isMainDesktopClosed){
-        mainDesktop.classList.add(inactive);
-    };
+    mainMobile.classList.add(inactive);
+    mainDesktop.classList.add(inactive);
+    productDetails.classList.add(inactive);
     shoppingCart.classList.toggle(inactive);
-};
+}
+
+function closeProductDetails(){
+    productDetails.classList.add(inactive);
+}
+
+function openProductDetails(){
+    mainMobile.classList.add(inactive);
+    mainDesktop.classList.add(inactive);
+    shoppingCart.classList.add(inactive);
+    productDetails.classList.remove(inactive);
+}
 
 function renderProduct(){
-    const productList = [];
+    // Array de productos
+    const productDB = [
+        ["Boh","120,00","./assets/images/boh.jpeg"],
+        ["Cubone","120,00","./assets/images/cubone.png"],
+        ["Dragonite","120,00","./assets/images/dragonite.jpg"],
+        ["Nezuko","120,00","./assets/images/nezuko.png"],
+        ["Pikachu","120,00","./assets/images/pikachu.png"],
+        ["Totoro","120,00","./assets/images/totoro.jpeg"],
+        ["Boh","120,00","./assets/images/boh.jpeg"],
+        ["Cubone","120,00","./assets/images/cubone.png"],
+        ["Dragonite","120,00","./assets/images/dragonite.jpg"],
+        ["Nezuko","120,00","./assets/images/nezuko.png"],
+        ["Pikachu","120,00","./assets/images/pikachu.png"],
+        ["Totoro","120,00","./assets/images/totoro.jpeg"]
+    ];
 
     // Template o base de un objeto
     function product(name, price, img){
         this.name = name,
         this.price = price,
         this.img = img
-    };
+    }
 
-    const productDB = [
-        ["Boh",120.00,"/assets/images/boh.jpeg"],
-        ["Cubone",120.00,"/assets/images/cubone.png"],
-        ["Dragonite",120.00,"/assets/images/dragonite.jpg"],
-        ["Nezuko",120.00,"/assets/images/nezuko.png"],
-        ["Pikachu",120.00,"/assets/images/pikachu.png"],
-        ["Totoro",120.00,"/assets/images/totoro.jpeg"],
-        ["Boh",120.00,"/assets/images/boh.jpeg"],
-        ["Cubone",120.00,"/assets/images/cubone.png"],
-        ["Dragonite",120.00,"/assets/images/dragonite.jpg"],
-        ["Nezuko",120.00,"/assets/images/nezuko.png"],
-        ["Pikachu",120.00,"/assets/images/pikachu.png"],
-        ["Totoro",120.00,"/assets/images/totoro.jpeg"]
-    ];
+    const productList = [];
 
     // Recorro el array productDB y convierto cada elemento del array en un objeto, luego guardo el objeto en otro array
     for(let i=0; i<productDB.length;i++){ 
         productList.push(new product(productDB[i][0],productDB[i][1],productDB[i][2]));
-    };
+    }
 
     // Maqueto el código HTML desde JavaScript
     for(prod of productList){
@@ -77,9 +86,10 @@ function renderProduct(){
         const productCardImg = document.createElement("img");
         productCardImg.setAttribute("src",prod.img);
         productCardImg.setAttribute("alt",prod.name);
+        productCardImg.addEventListener("click",openProductDetails);
 
         const productCardInfo = document.createElement("div");
-        productCardInfo.classList.add("product-info");
+        productCardInfo.classList.add("product-card-info");
 
         const productCardInfoDiv = document.createElement("div");
         const productCardInfoPrice = document.createElement("p");
@@ -99,7 +109,7 @@ function renderProduct(){
         productCardInfoDiv.appendChild(productCardInfoName);
         productCardInfo.appendChild(productCardInfoFigure);
         productCardInfoFigure.appendChild(productCardInfoFigureImg);
-        productContainer.appendChild(productCard);
-    };
-};
+        productCardContainer.appendChild(productCard);
+    }
+}
 renderProduct();
